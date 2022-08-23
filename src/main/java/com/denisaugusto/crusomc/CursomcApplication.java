f8a5f54779ffc4b532cccbb617d5601da7f47042
Denis Augusto <sinedps418@gmail.com>
2022-08-23 13:51:24 -0300

    Criando operacao de instaciacao

--- src/main/java/com/denisaugusto/crusomc/CursomcApplication.java
+++ src/main/java/com/denisaugusto/crusomc/CursomcApplication.java
@@ -1,14 +1,35 @@
 package com.denisaugusto.crusomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.denisaugusto.crusomc.domain.Categoria;
+import com.denisaugusto.crusomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
-}
+	@Override
+	public void run(String... args) throws Exception {
 
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
+
+}
+}
